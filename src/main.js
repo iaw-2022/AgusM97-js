e32c1ae2f827d65cad3eb0f45d58f2362cfc5c3d
AgusM97 <agus91997@gmail.com>
2022-05-31 21:23:32 -0300

    login

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createAuth0 } from "@auth0/auth0-vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: "dev-0f-5co-0.us.auth0.com",
+    client_id: "yJyGr32ely63D7pigU7bSI5h9GClY0fW",
+    redirect_uri: window.location.origin,
+    audience: "https://proyecto-IAW/api",
+  })
+);
+
+app.mount("#app");
